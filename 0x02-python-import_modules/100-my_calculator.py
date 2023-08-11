3348a381df93171124c805e320ccdd9835c58328
kiddingmz <bata.isidro@gmail.com>
2023-08-11 23:09:56 +0200

    STYLE: 6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
     import sys
 
-    length_args = len(sys.argv)
+    num_args = len(sys.argv)
 
-    if length_args != 4:
+    if num_args != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    operators = ('+', '-', '*', '/')
-    args = sys.argv
-    operator = args[2]
-
-    if operator not in operators:
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
+    a, b = int(sys.argv[1]), int(sys.argv[3])
 
-    a, b = int(args[1]), int(args[3])
-
-    if operator == operators[0]:
+    if op == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    elif operator == operators[1]:
+    elif op == '-':
         print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    elif operaror == operators[2]:
+    elif op == '*':
         print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     else:
         print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
