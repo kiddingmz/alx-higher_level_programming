ca1658ae3cc2d41a06de8635e1eb461d11dd958a
kiddingmz <bata.isidro@gmail.com>
2023-08-15 00:28:45 +0200

    STYLe: 6. print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        len_colums = len(matrix[i])
-        for j in range(len_colums):
-            separ = " "
-            if j == len_colums - 1:
-                separ = "\n"
-            print("{:d}".format(matrix[i][j]), end=separ)
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
