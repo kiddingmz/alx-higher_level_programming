89470ad95d38ce4484ce0e5209e1a78d9f3f4dbe
kiddingmz <bata.isidro@gmail.com>
2023-08-15 00:22:06 +0200

    CHORE: fix bug OS: 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -15,12 +15,13 @@ void print_python_list_info(PyObject *p)
 
 	size = Py_SIZE(p);
 	printf("[*] Size of the Python List = %ld\n", size);
+
 	list = (PyListObject *)p;
 	printf("[*] Allocated = %ld\n", list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
- 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
