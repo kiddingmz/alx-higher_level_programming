a1f9facf6a65ee4b741401f1e00fe219c8c36fff
kiddingmz <bata.isidro@gmail.com>
2023-08-29 20:20:15 +0200

    FEAT: 1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    def __init__(self, size):
+        """Initialization constrution.
+
+        Args:
+            size (int): size
+
+        Return:
+            nothing
+        """
+        self.__size = size
+    pass
