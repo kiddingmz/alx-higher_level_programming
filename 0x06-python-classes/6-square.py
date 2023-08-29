1f6fe67f5f5a6a5334b986982e8de50280137c5f
kiddingmz <bata.isidro@gmail.com>
2023-08-29 22:41:19 +0200

    STYLE: 6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -14,10 +14,8 @@ class Square:
         Return:
             nothing
         """
-        self.size(size)
-        self.position(position)
-        self.__size = self.size
-        self.__position = self.position
+        self.__size = size
+        self.__position = position
 
     def area(self) -> int:
         """Define the area for the square.
