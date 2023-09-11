f33477a420fbc38310da2c4924742347c32523a5
kiddingmz <bata.isidro@gmail.com>
2023-09-11 18:38:08 +0200

    STYLE: 3. Same class or inherit from

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -10,4 +10,6 @@ def is_kind_of_class(obj, a_class):
         True if obj is an instance of a_class
         False, otherwise
     """
-    return (isinstance(obj, a_class))
+    if isinstance(obj, a_class):
+        return (True)
+    return (False)
